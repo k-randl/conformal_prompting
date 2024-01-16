6d905612cd17d26123f46c6ea629ca1b8bf66a1e
Korbinian Randl <randlbem@web.de>
2024-01-17 00:19:21 +0100

    Fixed batch file

--- run_experiments.bat
+++ run_experiments.bat
@@ -1,33 +1,31 @@
-::@echo off
+@echo off
 
 set text_name=title
 set data_name=incidents
 set label_name=%1
 
-set arg=%2
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Naive:                                                                                         ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-python TrainerClassic.py bow-rnd %text_name% %label_name% %data_name% %arg%
-python TrainerClassic.py bow-sup %text_name% %label_name% %data_name% %arg%
+python TrainerClassic.py bow-rnd %text_name% %label_name% %data_name%
+python TrainerClassic.py bow-sup %text_name% %label_name% %data_name%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Bag-of-Words:                                                                                  ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-python TrainerClassic.py bow-knn %text_name% %label_name% %data_name% %arg%
-python TrainerClassic.py bow-lr  %text_name% %label_name% %data_name% %arg%
-python TrainerClassic.py bow-svm %text_name% %label_name% %data_name% %arg%
+python TrainerClassic.py bow-knn %text_name% %label_name% %data_name%
+python TrainerClassic.py bow-lr  %text_name% %label_name% %data_name%
+python TrainerClassic.py bow-svm %text_name% %label_name% %data_name%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: TF-IDF:                                                                                        ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-python TrainerClassic.py tfidf-knn %text_name% %label_name% %data_name% %arg%
-python TrainerClassic.py tfidf-lr  %text_name% %label_name% %data_name% %arg%
-python TrainerClassic.py tfidf-svm %text_name% %label_name% %data_name% %arg%
+python TrainerClassic.py tfidf-knn %text_name% %label_name% %data_name%
+python TrainerClassic.py tfidf-lr  %text_name% %label_name% %data_name%
+python TrainerClassic.py tfidf-svm %text_name% %label_name% %data_name%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Transformers:                                                                                  ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-python TrainerTransformer.py roberta-base %text_name% %label_name% %data_name% -e 20 -bs 16 -p 5 -nf softmax --shuffle %arg%
-python TrainerTransformer.py xlm-roberta-base %text_name% %label_name% %data_name% -e 20 -bs 16 -p 5 -nf softmax --shuffle %arg%
+python TrainerTransformer.py roberta-base %text_name% %label_name% %data_name% -e 20 -bs 16 -p 5 -nf softmax --shuffle
+python TrainerTransformer.py xlm-roberta-base %text_name% %label_name% %data_name% -e 20 -bs 16 -p 5 -nf softmax --shuffle
